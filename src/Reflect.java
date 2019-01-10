829c9af8c34c345958db53d768753e3849fe8684
Murique11 <chinadiaoyuisland617@gmail.com>
2019-01-10 16:12:25 +0800

    1013

---
+++ src/Reflect.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+@SuppressWarnings("all")
+public class Reflect {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> ints = new ArrayList();
+        ints.add(5);
+        ints.add(3);
+        Class aClass = ints.getClass();
+        aClass.getMethod("add", Object.class).invoke(ints,"String");
+        ints.add(4);
+        System.out.println(ints);
+
+    }
+}
